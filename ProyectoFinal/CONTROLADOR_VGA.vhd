49ec3f2937fa78e0566fbf9ccad006623edec1c7
Istanolion <akuma_diego@hotmail.com>
2019-11-18 12:47:20 -0600

    8 teclas

--- ProyectoFinal/CONTROLADOR_VGA.vhd
+++ ProyectoFinal/CONTROLADOR_VGA.vhd
@@ -6,7 +6,7 @@ use ieee.numeric_std.all;
 entity CONTROLADOR_VGA is
 port(	KEY : in std_logic_VECTOR (1 DOWNTO 0);
 		MAX10_CLK1_50 : in STD_LOGIC;
-		SW: in STD_LOGIC_VECTOR(4 downto 0);
+		SW: in STD_LOGIC_VECTOR(7 downto 0);
 		VGA_HS, VGA_VS : out std_logic;
 		VGA_R, VGA_G, VGA_B: out std_logic_vector(3 downto 0));
 end CONTROLADOR_VGA;
@@ -24,8 +24,10 @@ SIGNAL COLORNEGRO:  STD_LOGIC_VECTOR (11 DOWNTO 0):="000000000000";--Codigo RGB 
 SIGNAL COLORGRIS:   STD_LOGIC_VECTOR (11 DOWNTO 0):="101010101010";--Codigo RGB del Color gris
 
 --Para ver que columna se esta presionando se utiliza un integer con rango
-SIGNAL columActiv : integer range 0 to 5:=0; --numero cero= ninguna columna es presionada 
+SIGNAL columActiv : integer range 0 to 8:=0; --numero cero= ninguna columna es presionada 
 
+--Señal para especificar la señal al final
+SIGNAL RGBCOLORS : STD_LOGIC_VECTOR (11 DOWNTO 0);
 component SINC_VGA
 port (clk : in std_logic;
 		hsync, vsync, habilitado : out std_logic;
@@ -42,11 +44,14 @@ begin
 			div2 <= not div2;
 		end if;
 	end process;
-	with SW select columActiv<= 1 when "00001",
-		2 when "00010",
-		3 when "00100",
-		4 when "01000",
-		5 when "10000",
+	with SW select columActiv<= 1 when "00000001",
+		2 when "00000010",
+		3 when "00000100",
+		4 when "00001000",
+		5 when "00010000",
+		6 when "00100000",
+		7 when "01000000",
+		8 when "10000000",
 		0 when others;
 
 	senialVGA: process(div2)
@@ -54,73 +59,67 @@ begin
 			if rising_edge(div2) then
 				if habilitado = '1' then
 						IF (pos_y<360) then
-							IF(pos_x<127 and pos_x>=0) then
+							IF(pos_x<80 and pos_x>=0) then
 								if columActiv=1 then
-									VGA_R <= COLORGRIS(11 downto 8);
-									VGA_G <= COLORGRIS(7 downto 4);
-									VGA_B <= COLORGRIS(3 downto 0);
+									RGBCOLORS<=COLORGRIS;
 								ELSE
-									VGA_R <= COLORBLANCO(11 downto 8);
-									VGA_G <= COLORBLANCO(7 downto 4);
-									VGA_B <= COLORBLANCO(3 downto 0);
+									RGBCOLORS<=COLORBLANCO;
 								end if;
-							elsif (pos_x>128 and pos_x<255) then 
+							elsif (pos_x>81 and pos_x<159) then 
 								if columActiv=2 then
-									VGA_R <= COLORGRIS(11 downto 8);
-									VGA_G <= COLORGRIS(7 downto 4);
-									VGA_B <= COLORGRIS(3 downto 0);
+									RGBCOLORS<=COLORGRIS;
 								ELSE
-									VGA_R <= COLORBLANCO(11 downto 8);
-									VGA_G <= COLORBLANCO(7 downto 4);
-									VGA_B <= COLORBLANCO(3 downto 0);
+									RGBCOLORS<=COLORBLANCO;
 								end if;
-							elsif (pos_x<383 and pos_x>256) then
+							elsif (pos_x<239 and pos_x>160) then
 								if columActiv=3 then
-									VGA_R <= COLORGRIS(11 downto 8);
-									VGA_G <= COLORGRIS(7 downto 4);
-									VGA_B <= COLORGRIS(3 downto 0);
+									RGBCOLORS<=COLORGRIS;
 								ELSE
-									VGA_R <= COLORBLANCO(11 downto 8);
-									VGA_G <= COLORBLANCO(7 downto 4);
-									VGA_B <= COLORBLANCO(3 downto 0);
+									RGBCOLORS<=COLORBLANCO;
 								end if;
-							elsif (pos_x<511 and pos_x>385) then
+							elsif (pos_x<319 and pos_x>240) then
 								if columActiv=4 then
-									VGA_R <= COLORGRIS(11 downto 8);
-									VGA_G <= COLORGRIS(7 downto 4);
-									VGA_B <= COLORGRIS(3 downto 0);
+									RGBCOLORS<=COLORGRIS;
 								ELSE
-									VGA_R <= COLORBLANCO(11 downto 8);
-									VGA_G <= COLORBLANCO(7 downto 4);
-									VGA_B <= COLORBLANCO(3 downto 0);
+									RGBCOLORS<=COLORBLANCO;
 								end if;
-							elsif (pos_x>513) then
+							elsif (pos_x<399 and pos_x>320) then
 								if columActiv=5 then
-									VGA_R <= COLORGRIS(11 downto 8);
-									VGA_G <= COLORGRIS(7 downto 4);
-									VGA_B <= COLORGRIS(3 downto 0);
+									RGBCOLORS<=COLORGRIS;
 								ELSE
-									VGA_R <= COLORBLANCO(11 downto 8);
-									VGA_G <= COLORBLANCO(7 downto 4);
-									VGA_B <= COLORBLANCO(3 downto 0);
+									RGBCOLORS<=COLORBLANCO;
+								end if;
+							elsif (pos_x<479 and pos_x>400) then
+								if columActiv=6 then
+									RGBCOLORS<=COLORGRIS;
+								ELSE
+									RGBCOLORS<=COLORBLANCO;
+								end if;
+							elsif (pos_x<559 and pos_x>480) then
+								if columActiv=7 then
+									RGBCOLORS<=COLORGRIS;
+								ELSE
+									RGBCOLORS<=COLORBLANCO;
+								end if;
+							elsif (pos_x<640 and pos_x>560) then
+								if columActiv=8 then
+									RGBCOLORS<=COLORGRIS;
+								ELSE
+									RGBCOLORS<=COLORBLANCO;
 								end if;
 							else
-								VGA_R <= COLORNEGRO(11 downto 8);
-								VGA_G <= COLORNEGRO(7 downto 4);
-								VGA_B <= COLORNEGRO(3 downto 0);
+								RGBCOLORS<=COLORNEGRO;
 							end if;
 						else 
-							IF(pos_x/=127 and pos_x/=128 and pos_x/=255 and pos_x/=256 and pos_x/=383 and pos_x/=384 and pos_x/=511 and pos_x/=512) then
-								VGA_R <= COLORNEGRO(11 downto 8);
-								VGA_G <= COLORNEGRO(7 downto 4);
-								VGA_B <= COLORNEGRO(3 downto 0);
+							IF(pos_x/=80 and pos_x/=81 and pos_x/=160 and pos_x/=159 and pos_x/=240 and pos_x/=239 and pos_x/=320 and pos_x/=319 and pos_x/=400 and pos_x/=399 and pos_x/=480 and pos_x/=479 and pos_x/=560 and pos_x/=559) then
+								RGBCOLORS<=COLORNEGRO;
 							else
-								VGA_R <= COLORBLANCO(11 downto 8);
-								VGA_G <= COLORBLANCO(7 downto 4);
-								VGA_B <= COLORBLANCO(3 downto 0);
+								RGBCOLORS<=COLORBLANCO;
 							end if;
 						end if;
-						
+					VGA_R <= RGBCOLORS(11 DOWNTO 8);
+					VGA_G <= RGBCOLORS(7 DOWNTO 4);
+					VGA_B <= RGBCOLORS(3 DOWNTO 0);
 				else
 					VGA_R <= "0000";
 					VGA_G <= "0000";
