74017b201bf886af9fc85e44d877f915a1782bfc
Istanolion <akuma_diego@hotmail.com>
2019-11-18 18:02:15 -0600

    ALV ya ni yo le entiendo

--- ProyectoFinal/CONTROLADOR_VGA.vhd
+++ ProyectoFinal/CONTROLADOR_VGA.vhd
@@ -36,12 +36,12 @@ port (clk : in std_logic;
 end component;
 
 
-type rom_type is array (8 downto 0) of integer range 0 to 8; 
-type twoInts is array (1 downto 0) of integer range 0 to 8;
-Signal notas:rom_type:=(1,2,3,4,5,6,7,8,0);
+type rom_type is array (0 to 8) of integer range 0 to 8; 
+type twoInts is array (0 to 1) of integer range 0 to 8;
+Signal notas:rom_type:=(1,3,5,2,1,8,5,4,2);
 signal index: integer range 0 to 8:=0;
-signal playingnote: twoInts;
-signal mov_y1,mov_y2 : integer range 0 to 480 := 0;
+signal playingnote: twoInts := (1,1);
+signal mov_y1,mov_y2,mov_y3,mov_y4,mov_y5,mov_y6,mov_y7,mov_y8 : integer range 0 to 480 := 0;
 
 
 begin
@@ -74,8 +74,18 @@ begin
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
-								if (pos_y <mov_y1-320)then
-									RGBCOLORS<=COLORNEGRO;
+								if (playingnote(0)=1 or playingnote(1)=1) then
+									if (pos_y >mov_y1-160 and pos_y<mov_y1)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y1=160 ) then
+											if(playingnote(0)=1 and pos_X=79 ) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=1 and pos_X=79) then
+												playingnote(1)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
 								end if;
 							elsif (pos_x>81 and pos_x<159) then 
 								if columActiv=2 then
@@ -83,11 +93,37 @@ begin
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
+								if (playingnote(0)=2 or playingnote(1)=2) then
+									if (pos_y >mov_y2-160 and pos_y<mov_y2)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y2=160 ) then
+											if(playingnote(0)=2 and pos_X=158) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=2 and pos_X=158) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
+								end if;
 							elsif (pos_x<239 and pos_x>160) then
 								if columActiv=3 then
 									RGBCOLORS<=COLORGRIS;
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
+								end if;
+								if (playingnote(0)=3 or playingnote(1)=3) then
+									if (pos_y >mov_y3-160 and pos_y<mov_y3)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y3=160 ) then
+											if(playingnote(0)=3 and pos_X=238) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=3 and pos_X=238) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
 								end if;
 							elsif (pos_x<319 and pos_x>240) then
 								if columActiv=4 then
@@ -95,11 +131,37 @@ begin
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
+								if (playingnote(0)=4 or playingnote(1)=4) then
+									if (pos_y >mov_y4-160 and pos_y<mov_y4)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y4=160 ) then
+											if(playingnote(0)=4 and pos_X=318) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=4 and pos_X=318) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
+								end if;
 							elsif (pos_x<399 and pos_x>320) then
 								if columActiv=5 then
 									RGBCOLORS<=COLORGRIS;
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
+								end if;
+								if (playingnote(0)=5 or playingnote(1)=5) then
+									if (pos_y >mov_y5-160 and pos_y<mov_y5)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y5=160 ) then
+											if(playingnote(0)=5 and pos_X=398) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=5 and pos_X=398) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
 								end if;
 							elsif (pos_x<479 and pos_x>400) then
 								if columActiv=6 then
@@ -107,22 +169,61 @@ begin
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
+								if (playingnote(0)=6 or playingnote(1)=6) then
+									if (pos_y >mov_y6-160 and pos_y<mov_y6)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y6=160 ) then
+											if(playingnote(0)=6 and pos_X=478) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=6 and pos_X=478) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
+								end if;
 							elsif (pos_x<559 and pos_x>480) then
 								if columActiv=7 then
 									RGBCOLORS<=COLORGRIS;
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
-							elsif (pos_x<640 and pos_x>560) then
+								if (playingnote(0)=7 or playingnote(1)=7) then
+									if (pos_y >mov_y7-160 and pos_y<mov_y7)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y7=160 ) then
+											if(playingnote(0)=7 and pos_X=558) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=7 and pos_X=558) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
+								end if;
+							elsif (pos_x<639 and pos_x>560) then
 								if columActiv=8 then
 									RGBCOLORS<=COLORGRIS;
 								ELSE
 									RGBCOLORS<=COLORBLANCO;
 								end if;
+								if (playingnote(0)=8 or playingnote(1)=8) then
+									if (pos_y >mov_y8-160 and pos_y<mov_y8)then
+										RGBCOLORS<=COLORNEGRO;
+										if(mov_y8=160 ) then
+											if(playingnote(0)=8 and pos_X=639) then
+												playingnote(1)<=notas(index);
+											elsif (playingnote(1)=8 and pos_X=639) then
+												playingnote(0)<=notas(index);
+											end if;
+											
+										end if;
+									end if;
+								end if;
 							else
 								RGBCOLORS<=COLORNEGRO;
 							end if;
-							if (pos_x<640 and pos_x>439 AND pos_y<105 and pos_y>4) THEN
+							if (pos_x<639 and pos_x>439 AND pos_y<105 and pos_y>4) THEN
 								RGBCOLORS<=COLORNEGRO;
 							end if;
 						else 
@@ -140,8 +241,54 @@ begin
 					VGA_G <= "0000";
 					VGA_B <= "0000";
 				end if;
-				if pos_y=640 then
-					mov_y1<=mov_y1+1;
+				if pos_y=480 and pos_x=640 then
+					if (playingnote(0)=1 or playingnote(1)=1) then
+						mov_y1<=mov_y1+1;
+					else
+						mov_y1<=0;
+					end if;
+					if (playingnote(0)=2 or playingnote(1)=2) then
+						mov_y2<=mov_y2+1;
+					else
+						mov_y2<=0;
+					end if;
+					if (playingnote(0)=3 or playingnote(1)=3) then
+						mov_y3<=mov_y3+1;
+					else
+						mov_y3<=0;
+					end if;
+					if (playingnote(0)=4 or playingnote(1)=4) then
+						mov_y4<=mov_y4+1;
+					else
+						mov_y4<=0;
+					end if;
+					if (playingnote(0)=5 or playingnote(1)=5) then
+						mov_y5<=mov_y5+1;
+					else
+						mov_y5<=0;
+					end if;
+					if (playingnote(0)=6 or playingnote(1)=6) then
+						mov_y6<=mov_y6+1;
+					else
+						mov_y6<=0;
+					end if;
+					if (playingnote(0)=7 or playingnote(1)=7) then
+						mov_y7<=mov_y7+1;
+					else
+						mov_y7<=0;
+					end if;
+					if (playingnote(0)=8 or playingnote(1)=8) then
+						mov_y8<=mov_y8+1;
+					else
+						mov_y8<=0;
+					end if;
+					if(mov_y1=160 or mov_y2=160 or mov_y3=160 or mov_y4=160 or mov_y5=160 or mov_y6=160 or mov_y7=160 or mov_y8=160) then
+						if (index <8) then
+							index<=index+1;
+						else
+							index<=0;
+						end if;
+					end if;
 				end if;
 			end if;
 	end process;
